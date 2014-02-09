d2f5cd39e0e5c38b9157af830371508e9d6f6e0f
Andrew Brooks <none@ofyour.biz>
2014-02-08 20:49:20 -0500

    renamed:    gen/com/lodderlab/parking/BuildConfig.java -> gen/com/bignerdranch/android/runtracker/BuildConfig.java 	renamed:    gen/com/lodderlab/parking/R.java -> gen/com/bignerdranch/android/runtracker/R.java 	modified:   src/com/lodderlab/parking/MainActivity.java

--- gen/com/bignerdranch/android/runtracker/R.java
+++ gen/com/bignerdranch/android/runtracker/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package com.lodderlab.parking;
+package com.bignerdranch.android.runtracker;
 
 public final class R {
     public static final class attr {
