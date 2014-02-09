7e39becbbed22c797a83dfd6d776800f9ba97650
Andrew Brooks <none@ofyour.biz>
2014-02-08 21:48:01 -0500

    Changed API Min to include fragments.

--- src/com/lodderlab/parking/MainActivity.java
+++ src/com/lodderlab/parking/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+// import android.app.Fragment;
 
 public class MainActivity extends SingleFragmentActivity {
 
