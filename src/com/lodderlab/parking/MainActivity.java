1515bae37515fd1c8e6cfd17285d94e538ef6732
Joshua Miller <jpm4jc@gmail.com>
2014-02-08 22:23:19 -0500

    Added SingleFragmentActivity
    added SingleFragmentActivity

--- src/com/lodderlab/parking/MainActivity.java
+++ src/com/lodderlab/parking/MainActivity.java
@@ -10,7 +10,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
-// import android.app.Fragment;
 
 public class MainActivity extends SingleFragmentActivity {
 
